2ddb4dee29d2c0eba0a4e7dbd065202ea9dd528a
Rene Koens <mavy@ninjanerd.eu>
2024-08-08 17:50:34 +0200

    chore: better grepping
    Commit generated by whatthecommit.com
    
    Signed-off-by: Rene Koens <mavy@ninjanerd.eu>

--- apps/tdarr/ci/latest.sh
+++ apps/tdarr/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/users/haveagitgat/packages/container/tdarr/versions" | jq 'map( select( .metadata.container.tags[] | test("^latest$")  ) | .metadata.container.tags[] )' | jq 'map( select( index("latest") | not ) )'  | jq --raw-output '.[0]')
+version=$(curl -sX GET --header "Authorization: Bearer ${TOKEN}" "https://api.github.com/users/haveagitgat/packages/container/tdarr/versions" | jq 'map( select( .metadata.container.tags[] | test("^latest$")  ) | .metadata.container.tags[] )' | jq 'map( select( index("latest") | not ) )'  | jq --raw-output '.[0]')
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
